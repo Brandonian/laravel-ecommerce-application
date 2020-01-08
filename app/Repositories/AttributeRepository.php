72808e992cf1e220480f305225fd26e758f05354
Brandon Webster <bwwebs@gmail.com>
2020-01-08 13:08:03 -0700

    Add admin section for attribute values using VueJS (incomplete)

--- app/Repositories/AttributeRepository.php
+++ app/Repositories/AttributeRepository.php
@@ -105,4 +105,15 @@ class AttributeRepository extends BaseRepository implements AttributeContract
 
         return $attribute;
     }
+
+    public function getValues(Request $request)
+    {
+        $attributeId = $request->input('id');
+        $attribute = $this->attributeRepository->findAttributeById($attributeId);
+
+        $values = $attribute->values;
+
+        return response()->json($values);
+    }
 }
+
--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>@yield('title') - {{ config('app.name') }}</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') - {{ config('app.name') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('backend/css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}">
 </head>
 <body class="app sidebar-mini rtl">
     @include('admin.partials.header')
     @include('admin.partials.sidebar')
-    <main class="app-content">
+    <main class="app-content" id="app">
         @yield('content')
     </main>
     <script src="{{ asset('backend/js/jquery-3.2.1.min.js') }}"></script>
@@ -19,5 +20,6 @@
     <script src="{{ asset('backend/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('backend/js/main.js') }}"></script>
     <script src="{{ asset('backend/js/plugins/pace.min.js') }}"></script>
+    @stack('scripts')
 </body>
 </html>
